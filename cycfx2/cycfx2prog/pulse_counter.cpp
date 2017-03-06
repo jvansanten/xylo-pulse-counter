4bb4b452f5babacaadc4c83e6dfc412c6c8e8314
Jakob van Santen <jvansanten@gmail.com>
2017-03-06 15:39:57 +0100

    Use static FX2 firmware

--- cycfx2/cycfx2prog/pulse_counter.cpp
+++ cycfx2/cycfx2prog/pulse_counter.cpp
@@ -24,10 +24,35 @@ enum FPGACommand {
 };
 
 struct count_statistics {
-	std::vector<int> counts;
-	int cycles;
+	std::vector<unsigned int> counts;
+	unsigned int cycles;
 	
 	count_statistics() : counts(4,0), cycles(0) {};
+};
+
+static const char* xylo_setup[] = {
+	":03000000020006F5\n",
+	":03005F0002000399\n",
+	":030003000201AA4D\n",
+	":2000620090E680E0FF74085FF0E0FF74025FF090E6007412F00000000090E60174E3F00090\n",
+	":2000820000000090E60B7403F00000000090E6027498F00000000090E60374FEF000000027\n",
+	":2000A2000090E61274A2F090E61374A0F090E61474E2F090E61574E0F0000000001200D2A0\n",
+	":2000C20075A80053D8DF90E680E0FF74F75FF02290E6047480F00000000090E6047482F088\n",
+	":2000E2000000000090E6047484F00000000090E6047486F00000000090E6047488F00000D2\n",
+	":20010200000090E604E4F00000000090E618E4F00000000090E6497482F00000000090E612\n",
+	":20012200497482F00000000090E619E4F00000000090E6497484F00000000090E6497484CD\n",
+	":20014200F00000000090E61AE4F00000000090E6487406F00000000090E6487406F00000F9\n",
+	":20016200000090E61BE4F00000000090E6487408F00000000090E6487408F0000000009034\n",
+	":20018200E6187410F00000000090E6197410F00000000090E61A740CF00000000090E61B57\n",
+	":0D01A200740CF0000000002212006280FECC\n",
+	":06003500E478FFF6D8FD9F\n",
+	":200013007900E94400601B7A009001B3780175A000E493F2A308B8000205A0D9F4DAF275DF\n",
+	":02003300A0FF2C\n",
+	":20003B007800E84400600A790175A000E4F309D8FC7800E84400600C7900900001E4F0A3C3\n",
+	":04005B00D8FCD9FAFA\n",
+	":0D0006007581071201AFE58260030200035F\n",
+	":0401AF007582002233\n",
+	":00000001FF\n"
 };
 
 class PulseCounter : private CypressFX2Device {
@@ -38,35 +63,37 @@ public:
 		CypressFX2Device::open(usbdev);
 		
 		CypressFX2Device::FX2Reset(0);
-		CypressFX2Device::ProgramIHexFile("xylo_setup.ihx");
+		CypressFX2Device::ProgramStaticIHex(xylo_setup);
 		CypressFX2Device::FX2Reset(1);
 	}
 	PulseCounter(const PulseCounter &) = delete;
-	
-	count_statistics count() {
-		count_statistics stats;
-		
+
+private:
+	void request_counts() {
 		// Write a command for the FPGA to EP2
-		{
-			unsigned char command = FPGA_ENABLE+FPGA_GETDATA;
-			CypressFX2Device::BlockWrite(0x02, &command, 1);
-		}
+		unsigned char command = FPGA_ENABLE+FPGA_GETDATA;
+		CypressFX2Device::BlockWrite(0x02, &command, 1);
+	}
+	
+	size_t next_packet_size() {
+		std::vector<unsigned char> buffer(512, 0);
+		int bytes = CypressFX2Device::BlockRead(0x88, buffer.data(), 512, 'B');
+		return (size_t(buffer[0])<<8) + size_t(buffer[1]);
+	}
+	
+	count_statistics read_counts() {
+		count_statistics stats;
 		
 		// Check size of packet in output by reading from EP8
 		// (not strictly neccessary, as the packet should always be 64 bytes)
-		size_t packet_size = 0;
-		{
-			std::vector<unsigned char> buffer(512, 0);
-			int bytes = CypressFX2Device::BlockRead(0x88, buffer.data(), 512, 'B');
-			packet_size = int(buffer[0]<<8) + int(buffer[1]);
-		}
+		size_t packet_size = next_packet_size();
 		if (packet_size == 0)
 			return stats;
 		
 		// Now, read the data from EP6
 		std::vector<unsigned char> buffer(512, 0);
 		int bytes = CypressFX2Device::BlockRead(0x86, buffer.data(), 512, 'B');
-		if (bytes > packet_size)
+		if (bytes < packet_size)
 			return stats;
 		
 		for (int offset=0; offset < bytes; offset += 64) {
@@ -89,8 +116,13 @@ public:
 		
 		return stats;
 	}
-	
-private:
+
+public:
+	count_statistics count() {
+		request_counts();
+		return read_counts();
+	}
+
 };
 
 void county(int iterations=20, bool quick=false)
@@ -102,7 +134,7 @@ void county(int iterations=20, bool quick=false)
 		auto counts = counter.count();
 		std::cout << i << ": " << counts.counts[0]/(counts.cycles/48e6) << " in " << counts.cycles << " ("<<counts.cycles/46e6<<" s)" << std::endl;
 	    using namespace std::chrono_literals;
-	    std::this_thread::sleep_for(1ms);
+	    std::this_thread::sleep_for(500ms);
 	}
 }
 
